1e989c12ee1f3247b4912b631c2531eee313ebe0
Alexander Makarov <sam@rmcreative.ru>
2017-11-03 01:55:57 +0300

    prepare for next release

---
+++ backend/controllers/BrandController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LBC
+ * Date: 2017/11/3
+ * Time: 15:57
+ */
+
+namespace backend\controllers;
+
+
+use yii\web\Controller;
+
+class BrandController extends Controller
+{
+
+    public function actionInedex()
+    {
+        echo 1111;
+
+    }
+
+}
